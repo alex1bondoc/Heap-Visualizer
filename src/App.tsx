a9deafead8812bed1c0a32ccc875665e3155280e
alex1bondoc <alex1bondoc@gmail.com>
2026-03-12 18:40:35 +0200

    Done for today

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Heap from './Components/Heap'
 import Header from './Components/Header'
 function App() {
@@ -9,7 +7,6 @@ function App() {
     <div className="flex flex-col h-screen w-full bg-slate-900  ">
         <Header size={heapSize}></Header>
         <Heap size={heapSize}></Heap>
-
     </div>
   )
 }
