7d9bccdb5da6d84074cd12bc2ee91edaaa616e67
alex1bondoc <alex1bondoc@gmail.com>
2026-03-12 17:23:23 +0200

    Solved problem with root

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Heap from './Components/Heap'
 import Header from './Components/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-screen bg-slate-900 p-10 flex flex-col items-center justify-center font-sans">
+    <div className="flex flex-col h-screen w-full bg-slate-900  ">
         <Header></Header>
         <Heap></Heap>
     </div>
