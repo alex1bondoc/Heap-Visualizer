dba95268f6605fa21395995603ceb5e673c015af
alex1bondoc <alex1bondoc@gmail.com>
2026-03-17 12:56:07 +0200

    Implemented input for the size of malloc

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import createHeapModule from './heap.js';
 
 function App() {
     const heapSize = 1024
+    const [mallocSize, setMallocSize] = useState<number>(0);
     const [blocks, setBlocks] = useState<MemoryBlock[]>([])
     const [wasmInstance, setWasmInstance] = useState<any>(null)
   
@@ -32,7 +33,7 @@ function App() {
         if (wasmInstance === null) {
             return;
         }
-        const serializedHeap = instance._wasmGetHeap();
+        const serializedHeap = instance.ccall('wasmGetHeap', ['string'], [], []);
         const jsonString = instance.UTF8ToString(serializedHeap);
         const json = JSON.parse(jsonString);
         setBlocks(json.map((block: any) => {return new MemoryBlock(block.id, block.size, block.status as Status)}));
@@ -41,7 +42,7 @@ function App() {
         if (instance === null) {
             return;
         }
-        instance._wasmMalloc(128);
+        instance.ccall('wasmMalloc', null, ['number'], [mallocSize]);
         refreshBlocks(instance);
     }
     const free = (instance: any, hexAddres: string) => {
@@ -51,11 +52,17 @@ function App() {
         instance._wasmFree(hexAddres);
         refreshBlocks(instance);
     }
+
+    const handleChange = (event: any) => {
+        const value = event.target.value;
+        setMallocSize(value);
+    }
     return (
         <div className="flex flex-col h-screen w-full bg-slate-900  ">
             <Header size={heapSize}></Header>
             <Heap size={heapSize} blocks={blocks}></Heap>
             <button onClick={() => malloc(wasmInstance)} className="border-2 bg-amber-400 h-12 w-20 rounded-xl">malloc</button>
+            <input className="h-12 w-25 bg-white rounded-xl" value={mallocSize} onChange={handleChange} ></input>
         </div>
     )
 }
