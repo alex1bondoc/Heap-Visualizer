0c3c6dc1869b61e01d0c7f48f911bcd36388e45c
alex1bondoc <alex1bondoc@gmail.com>
2026-03-19 23:37:09 +0200

    Debug easy

--- src/Components/SelectedBlockPanel.tsx
+++ src/Components/SelectedBlockPanel.tsx
@@ -10,12 +10,13 @@ export function SelectedBlockPanel({block}:{block: MemoryBlock}) {
         setSize(value);
     }
     return (
-        <div className="w-100 h-25 flex flex-col bg-slate-700 border-4 rounded-xl">
-            <p>Status: {block.status}</p>
-            <p>Size: {block.size}</p>
+        <div className="w-100 h-35 flex flex-col bg-white border-4">
+            <p className="mx-auto">Address: {block.id}</p>
+            <p className="mx-auto">Status: {block.status}</p>
+            <p className="mx-auto">Size: {block.size}</p>
             <button onClick={() => free(block.id)} className="bg-amber-400">Free Block</button>
-            <input onChange={handleChange} value={size}></input>
-            <button onClick={() =>  realloc(block.id, size)} className="bg-amber-400"></button>
+            <input className="mx-auto" onChange={handleChange} value={size}></input>
+            <button onClick={() =>  realloc(block.id, size)} className="bg-amber-400">Realloc</button>
         </div>
     )
 }
